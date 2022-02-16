a5ddaa9b78a5ae78b8f8301bc54be2b1bfc5d06a
Matt Pell <matt.pell@fotf.org>
2022-02-16 12:12:56 -0700

    refactor game tests

--- BowlingGameTests/GameTests.cs
+++ BowlingGameTests/GameTests.cs
@@ -7,38 +7,57 @@ namespace BowlingGameTests
 {
     public class GameTests
     {
+        private readonly Game game;
+        public GameTests()
+        {
+            game = new();
+        }
+
         [Fact]
         public void WhenAllGutterBallsAreThrown_GameScoreShouldBeZero()
         {
             //arrange
-            Game Game = new();
 
             //act
-            for (var i = 0; i < 20; i++)
-            {
-                Game.Roll(0);
-            }
-            var finalScore = Game.Score();
+            ThrowRolls();
 
             //assert
-            Assert.Equal(0, finalScore);
+            Assert.Equal(0, game.Score());
         }
 
         [Fact]
         public void WhenOnePinPerRollIsKnockedDown_GameScoreShouldBeTwenty()
         {
             //arrange
-            Game Game = new();
 
             //act
-            for (var i = 0; i < 20; i++)
-            {
-                Game.Roll(1);
-            }
-            var finalScore = Game.Score();
+            ThrowRolls(scorePerRoll: 1);
 
             //assert
-            Assert.Equal(20, finalScore);
+            Assert.Equal(20, game.Score());
+        }
+
+        [Fact]
+        public void WhenASpareIsThrownInFirstFrameAndTheFirstRollOfNextFrameIs3RemainingRollsAreGutters_GameScoreShouldBe16()
+        {
+            //arrange
+
+            //act
+            game.Roll(0);
+            game.Roll(10);
+            game.Roll(3);
+            ThrowRolls(17);
+
+            //assert
+            Assert.Equal(16, game.Score());
+        }
+
+        private void ThrowRolls(int numberOfRolls = 20, int scorePerRoll = 0)
+        {
+            for (var i = 0; i < numberOfRolls; i++)
+            {
+                game.Roll(scorePerRoll);
+            }
         }
     }
 }
